26a57375d352a7713886beaf99a8818d09db1ae4
Developer2 <adibaltab7860@gmail.com>
2025-08-17 05:45:25 +0530

    tl modified

--- src/com/nt/entity/Employee.java
+++ src/com/nt/entity/Employee.java
@@ -2,4 +2,57 @@ package com.nt.entity;
 
 public class Employee {
 
+	private Integer id;
+
+	private String name;
+
+	private String emailId;
+
+	private String phno;
+
+	public Employee(Integer id, String name, String emailId, String phno) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.emailId = emailId;
+		this.phno = phno;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPhno() {
+		return phno;
+	}
+
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", emailId=" + emailId + ", phno=" + phno + "]";
+	}
+
 }
---
+++ src/com/nt/tester/MainMethod.java
@@ -0,0 +1,15 @@
+package com.nt.tester;
+
+import com.nt.entity.Employee;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee(101, "Adib", "adibaltab@", "7980231027");
+		System.out.println(emp);
+		
+
+	}
+
+}
